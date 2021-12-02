b406e5c2d31189169e4903d7749131f35342ba0a
Pabs <pscola@laurentian.ca>
2021-12-02 02:17:47 -0500

    Created Initial Template

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gamecube-template
 A template for a devkitPro PowerPC environment in VS Code
+
+Run Tasks to make debug or release builds, or run debug or release build in Dolphin.
+Make sure all paths are correct in .vscode files. Must have full Dolphin emu in directory with all associated files.
---
+++ source/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include <ogcsys.h>
+#include <gccore.h>
+
+static u32 *xfb;
+static GXRModeObj *rmode;
+
+
+void Initialise() {
+  
+	VIDEO_Init();
+	PAD_Init();
+ 
+	rmode = VIDEO_GetPreferredMode(NULL);
+
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+ 
+	VIDEO_Configure(rmode);
+	VIDEO_SetNextFramebuffer(xfb);
+	VIDEO_SetBlack(FALSE);
+	VIDEO_Flush();
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+}
+
+
+int main() {
+ 
+	Initialise();
+ 
+	printf("Hello World!\n");
+	
+	while(1) {
+		u16 buttonsDown = PAD_ButtonsDown(0);
+	
+		if(buttonsDown & PAD_BUTTON_A) {
+			printf("Button A pressed.\n");
+		}
+		
+		u16 buttonsUp = PAD_ButtonsUp(0);
+		
+		if(buttonsUp & PAD_BUTTON_A) {
+			printf("Button A released.\n");
+		}
+		
+		if(PAD_StickY(0) > 18) {
+			printf("Joystick moved up.\n");
+		}
+		
+		if(PAD_StickY(0) < -18) {
+			printf("Joystick moved down.\n");
+		}
+		
+		//
+		if(buttonsDown & PAD_BUTTON_START) {
+			exit(0);
+		}
+	}
+	
+	return 0;
+}
